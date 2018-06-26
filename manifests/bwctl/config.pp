ca094a9ac6d114eae10f07b90f0b8f435bea16da
Robert Frank <rwf14f@gmail.com>
2018-06-26 12:00:53 +0100

    Add bwctl logging configuration.

--- manifests/bwctl/config.pp
+++ manifests/bwctl/config.pp
@@ -1,9 +1,12 @@
 class perfsonar::bwctl::config(
-  $iperf_port  = '5001-5300',
-  $nuttcp_port = '5301-5600',
-  $owamp_port  = '5601-5900',
-  $peer_port   = '6001-6200',
-  $test_port   = '5202-5900',
+  $iperf_port   = '5001-5300',
+  $nuttcp_port  = '5301-5600',
+  $owamp_port   = '5601-5900',
+  $test_port    = '5001-5900',
+  $peer_port    = '6001-6200',
+  $log_facility = 'local5',
+  $log_level    = 'error',
+  $log_alevel   = 'info',
 ) inherits perfsonar::params {
 
   file { '/etc/bwctl-server/bwctl-server.conf':
