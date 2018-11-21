552ef08283571f52ee5c4016f0e20b44a6b879fc
Robert Frank <robert.frank@manchester.ac.uk>
2018-11-21 14:45:10 +0000

    Prepare owamp configuration to be used with twamp.

--- manifests/owamp/config.pp
+++ manifests/owamp/config.pp
@@ -2,6 +2,10 @@ class perfsonar::owamp::config(
   Tuple[Stdlib::Port::Unprivileged, Stdlib::Port::Unprivileged] $test_port = [8760, 9960],
   String $log_facility = 'local5',
   Boolean $log_verbose = true,
+  String $user = 'owamp',
+  String $group = 'owamp',
+  Stdlib::Absolutepath $datadir = '/var/lib/owamp',
+  Optional[Float[1.0, 10.0]] $diskfudge = 3.0,
   Array[Perfsonar::Limits] $limits = [{
       name            => 'root',
       disk            => 0,
@@ -17,12 +21,14 @@ class perfsonar::owamp::config(
   String $default = 'regular',
 ) {
 
+  # owamp and twamp configurations are very simiar,
+  # therefore they use the same configuration file templates
   file { '/etc/owamp-server/owamp-server.conf':
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template("${module_name}/owamp-server.conf.erb"),
+    content => template("${module_name}/towamp-server.conf.erb"),
     require => Package['owamp-server'],
     notify  => Service['owamp-server'],
   }
@@ -31,7 +37,7 @@ class perfsonar::owamp::config(
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template("${module_name}/owamp-server.limits.erb"),
+    content => template("${module_name}/towamp-server.limits.erb"),
     require => Package['owamp-server'],
     notify  => Service['owamp-server'],
   }
